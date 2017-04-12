139adcdaf1cb3d38749e2b3dfd985c6e9e1c5bed
Evangelina Fiorucci <efiorucci@gmail.com>
2017-04-12 11:29:22 -0300

    new crackepop program

---
+++ CracklePop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+for x in range(1, 101):
+    if x % 15 == 0:
+        print('CracklePop')
+    elif x % 3 == 0:
+        print('Crackle')
+    elif x % 5 == 0:
+        print('Pop')
+    else:
+        print(x)
+
+print('Thank you and good bye!')
