bc6e659d09464f48c0657778a3ef15a4e7bac873
Evangelina Fiorucci <evangelinaf@users.noreply.github.com>
2017-06-26 19:43:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-    return "Welcome to your first containerized web app!
+    return "Hello World!\aWelcome to your first containerized web app!"    
 
 if __name__ == "__main__":
     app.run()
